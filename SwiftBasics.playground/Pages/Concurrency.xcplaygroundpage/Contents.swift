d0cb94390446b42df3163601208eda8e907b5ca6
David Dhinakaran J <daviddhinakaran100@gmail.com>
2026-01-23 12:17:23 +0530

    Learned Concurrency

--- SwiftBasics.playground/Pages/Concurrency.xcplaygroundpage/Contents.swift
+++ SwiftBasics.playground/Pages/Concurrency.xcplaygroundpage/Contents.swift
@@ -1,6 +1,6 @@
 //async and await
 func performTask(id: Int) async -> String {
-    try! await Task.sleep(for: .seconds(1))
+    try? await Task.sleep(for: .seconds(id))
     return "Task\(id) Completed"
 }
 
@@ -16,7 +16,7 @@ let numbers = [1, 2, 3, 4, 5]
 let squaredNumbers = AsyncStream<Int> { continuation in
     Task {
         for number in numbers {
-            try! await Task.sleep(for: .seconds(2))
+            try! await Task.sleep(for: .seconds(1))
             continuation.yield(number * number)
         }
         continuation.finish()
@@ -31,96 +31,120 @@ Task {
 
 //Calling Asynchronous Functions in Parallel
 Task {
-    async let result1 = await performTask(id: 1)
-    async let result2 = await performTask(id: 2)
+    async let result1 = performTask(id: 1)
+    async let result2 = performTask(id: 2)
     await print(result1)
     await print(result2)
 }
 
-////Structured Concurrency
-////async let
-//func parentTask() async {
-//    print("Parent Task Started")
-//    
-//    async let child1: Void = childTask(id: 1)
-//    async let child2: Void = childTask(id: 2)
-//    
-//    await child1
-//    await child2
-//    print("Parent Task Completed")
-//}
-//
-//func childTask(id: Int) async {
-//    print("Child Task with id \(id) Started")
-//    try? await Task.sleep(for: .seconds(2))
-//    print("Child Task with id \(id) Completed")
-//}
-//
-//Task {
-//    await parentTask()
-//}
-//
-////Task Group
-//func parentTask2() async {
-//    print("Parent Task started")
-//
-//    await withTaskGroup(of: Void.self) { group in
-//        for id in 3...5 {
-//            group.addTask {
-//                await childTask(id: id)
-//            }
-//        }
-//    }
-//
-//    print("Parent Task finished")
-//}
-//
-//Task {
-//    await parentTask2()
-//}
-//
-////Actors
-//actor Counter {
-//    var count: Int = 0
-//    var sum: Int = 0
-//    
-//    func increment() async {
-//        count += 1
-//        let c = count
-//        try? await Task.sleep(for: .nanoseconds(c))
-//        sum += c
-//    }
-//    
-//    func getCount() -> Int {
-//        return count
-//    }
-//    
-//    func getSum() -> Int {
-//        return sum
-//    }
-//}
-//
-//Task {
-//    let counter = Counter()
-//    
-//    await withTaskGroup(of: Void.self) { group in
-//        for _ in 0..<1000 {
-//            group.addTask {
-//                await counter.increment()
-//            }
-//        }
-//    }
-//    
-//    print(await counter.getCount())
-//    print(await counter.getSum())
-//}
-//
-////Main Actor - Processed by Main Thread
-//@MainActor
-//func performMainActorTask() async {
-//    print("This is executed in the main actor context")
-//}
-//
-//Task {
-//    await performMainActorTask()
-//}
+//Structured Concurrency
+//Task Groups
+Task {
+    await withTaskGroup(of: String.self) { group in
+        for id in 3...5 {
+            group.addTask {
+                await performTask(id: id)
+            }
+        }
+        
+        for await result in group {
+            print(result)
+        }
+    }
+}
+
+//Task Cancellation
+let sumOfSquaresTask = Task {
+    try await withThrowingTaskGroup(of: Int.self) { group in
+        for num in 1...10 {
+            group.addTaskUnlessCancelled {
+                try await Task.sleep(for: .seconds(num))
+                return num * num
+            }
+        }
+        var result: Int128 = 0
+        for try await num in group {
+            result &+= Int128(num)
+        }
+        return result
+    }
+}
+
+Task {
+    try! await Task.sleep(for: .seconds(6))
+    sumOfSquaresTask.cancel()
+}
+
+Task {
+    do {
+        let total = try await sumOfSquaresTask.value
+        print("Total: \(total)")
+    } catch is CancellationError {
+        print("Task was cancelled successfully.")
+    }
+}
+
+//Unstructured Concurrency
+func load() {
+    //This task continues running even after load() returns.
+    let task = Task {
+        while !Task.isCancelled {
+            try await Task.sleep(for: .seconds(1))
+            print("Working...")
+        }
+    }
+    
+    Task {
+        try await Task.sleep(for: .seconds(5))
+        task.cancel()
+    }
+}
+
+load()
+
+//The Main Actor
+@MainActor
+func performMainActorTask() async {
+    print("This is executed in the main actor context")
+}
+
+Task {
+    await performMainActorTask()
+}
+
+@MainActor
+protocol MainActorProtocol {}
+
+struct MainActorStructure: MainActorProtocol {}  //Implicitly @MainActor
+
+//Actors
+actor Counter {
+    var count: Int = 0
+    let by: Int = 2
+    
+    func increment() {
+        count += by
+    }
+    
+    func getCount() -> Int {
+        return count
+    }
+}
+
+var counter = Counter()
+counter.by
+Task {
+    await counter.increment()
+    await counter.getCount()
+}
+
+//Global Actors
+@globalActor
+actor GlobalCounter: GlobalActor {
+    static let shared = Counter()
+}
+
+Task { @GlobalCounter in
+    await GlobalCounter.shared.increment()
+    await GlobalCounter.shared.getCount()
+}
